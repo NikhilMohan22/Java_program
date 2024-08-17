f6dcc84f748df6651b62658d82986478f0318fba
NikhilMohan22 <nikhilmohanan22@gmail.com>
2024-08-17 14:28:38 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+package Test1;
+
+public class Multiplication {
+	public static void main(String[] args) {
+	
+ for(int j=2;j<=10;j++) {
+	 System.out.println("<"+j+">");
+	 for (int i=1;i<=10;i++) {
+		 System.out.println(i+"x"+j+"="+i*j);
+	 }
+ }
+
+}}
